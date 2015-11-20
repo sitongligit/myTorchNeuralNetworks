4d6944bdbe74031ce975f5d1af888f7897d7037d
jmrf <jose.marcos.rf@gmail.com>
2015-11-20 19:46:57 +0000

    Added weight decay, checkpoints and epoch control
    Pending:
    - num of layers
    - dropout
    - gpu support
    - documentation
    - testing (tottem)
    - interactive plotting while training

--- simple_LSTM.lua
+++ simple_LSTM.lua
@@ -46,28 +46,27 @@ function createLSTM(input_size, rnn_size)
     local prev_c = inputs[2]
     local prev_h = inputs[3]
 
-    -- connections: input --> hidden and hidden --> hidden
+    -- new input sum. connections: input --> hidden and hidden --> hidden
     local i2h = nn.Linear(input_size, 4 * rnn_size)(x)
     local h2h = nn.Linear(rnn_size, 4 * rnn_size)(prev_h)
     local preactivations = nn.CAddTable()({i2h, h2h})
-
-    -- gates
-    local pre_sigmoid_chunk = nn.Narrow(2, 1, 3 * rnn_size)(preactivations)
-    local all_gates = nn.Sigmoid()(pre_sigmoid_chunk)
 
     -- input
     local in_chunk = nn.Narrow(2, 3 * rnn_size + 1, rnn_size)(preactivations)
     local in_transform = nn.Tanh()(in_chunk)
 
+    -- gates
+    local pre_sigmoid_chunk = nn.Narrow(2, 1, 3 * rnn_size)(preactivations)
+    local all_gates = nn.Sigmoid()(pre_sigmoid_chunk)
     local in_gate = nn.Narrow(2, 1, rnn_size)(all_gates)
     local forget_gate = nn.Narrow(2, rnn_size + 1, rnn_size)(all_gates)
     local out_gate = nn.Narrow(2, 2 * rnn_size + 1, rnn_size)(all_gates)
 
+    -- next carrousel state: transform current cell and gated out
     local next_c = nn.CAddTable()({
         nn.CMulTable()({forget_gate, prev_c}),
         nn.CMulTable()({in_gate, in_transform})
     })
-
     local c_transform = nn.Tanh()(next_c)
     local next_h = nn.CMulTable()({out_gate, c_transform})
 
@@ -79,8 +78,20 @@ function createLSTM(input_size, rnn_size)
 end
 
 
+function LSTM.saveModel(self, epoch)
+    print('Checkpointing. Calculating validation accuracy..')
+    local val_acc = 1 - self:validate()
+    local savefile = string.format('%s/%s_epoch%.2f_%.4f.t7', self.opt.checkpoint_dir, self.opt.savefile, epoch, val_acc)
+    print('Saving checkpoint to ' .. savefile .. '\n')
+    local checkpoint = {}
+    checkpoint.opt = self.opt
+    checkpoint.protos = protos
+    torch.save(savefile, checkpoint)
+end
 
-function LSTM.buildLSTM(self, input_size, output_size)
+
+
+function LSTM.createRNN(self, input_size, output_size)
     -- create the model. Feed one input at a time (one value on the time series)
     self.protos = {}
     self.protos.lstm = createLSTM(input_size,self.opt.rnn_size)
@@ -132,7 +143,7 @@ function LSTM.train(self)
         for t = 1, self.opt.window_size do
             -- get specific time-step (select yields a batch_size x features matrix)
             local input_t = input:select(input:dim(),t)
-            if input_t:dim() == 1 then input_t = input_t:reshape(1,input_t:size(1)) end
+            if input_t:dim() == 1 then input_t = input_t:reshape(1,input_t:size(1)):t() end
 
             -- forward propagate for every every time-step
             -- note the curly braces around the function call (to return a table)
@@ -165,6 +176,7 @@ function LSTM.train(self)
             table.insert(drnn_state[t-1], dlst[3])
         end
 
+        -- clamp params to avoid the vanishing or exploding gradient problems 
         _, lstm_dparams = self.protos.lstm:getParameters()
         lstm_dparams:clamp(-5, 5)
 
@@ -179,20 +191,41 @@ function LSTM.train(self)
     losses = {}
     lloss = 0
 
+    local num_batches = self.loader.train_size / self.opt.batch_size
+    local iterations = self.opt.max_epochs * num_batches
+
     -- iterate for all the epochs
-    for i = 1, self.opt.max_epochs do
+    for i = 1, iterations do
+
+        local epoch = i / num_batches
+
         _,local_loss = optim.rmsprop(feval, self.params, optim_state)
         losses[#losses + 1] = local_loss[1]
-
         lloss = lloss + local_loss[1]
 
-        xlua.progress(i, self.opt.max_epochs)
+        xlua.progress(i, iterations)
 
+        -- print and garbage collect every now and then
         if i%10 == 0 then
-            -- print('epoch ' .. i .. ' loss ' .. lloss / 10)
+            -- print('epoch ' .. epoch .. ' loss ' .. lloss / 10)
             lloss = 0
             collectgarbage()
         end
+
+        -- learning rate decay
+        if i % num_batches == 0 and opt.learning_rate_decay < 1 then
+            if epoch >= opt.learning_rate_decay_after then
+                local decay_factor = opt.learning_rate_decay
+                optim_state.learningRate = optim_state.learningRate * decay_factor -- decay it
+                print('decayed learning rate by a factor ' .. decay_factor .. ' to ' .. optim_state.learningRate)
+            end
+        end
+
+        -- checkpoint: saving model
+        if i % opt.save_every == 0 or i == iterations then
+            self:saveModel(epoch)
+        end        
+
     end
 
     tensored_loss = torch.zeros(#losses)
@@ -205,7 +238,7 @@ function LSTM.train(self)
 end
 
 
-function LSTM.validate(self)
+function LSTM.validate(self, draw)
 
     ------------------- evaluation function enclosure -------------------
     local function feval_val()
@@ -217,10 +250,7 @@ function LSTM.validate(self)
         -- go through all the time series
         for t = 1, self.opt.window_size do
             local input = x:select(x:dim(),t)
-            if input:dim() == 1 then input = input:reshape(1,input:size(1)) end
-
-            -- print('Input:')
-            -- print(input)
+            if input:dim() == 1 then input = input:reshape(1,input:size(1)):t() end
 
             lst = self.protos.clones.lstm[t]:forward{input, unpack(rnn_state[t-1])}
             rnn_state[t] = {}
@@ -230,35 +260,39 @@ function LSTM.validate(self)
         -- propagate through the top layer the output of the last time-step
         predictions = self.protos.top:forward(lst[#lst])
 
-        -- print('target:')
-        -- print(y)
-        -- print('Prediction:')
-        -- print(predictions)
-        -- io.read()
+        -- forward through the criterion
+        loss = self.protos.criterion:forward(predictions, y)
 
-        return predictions, y
+        return predictions, y, loss
     end
     ------------------- evaluation function enclosure -------------------
 
-    print('Validating and drawing predictions')
-    prediction = torch.zeros(self.loader.validation_size*self.opt.batch_size)
-    gt = torch.zeros(self.loader.validation_size*self.opt.batch_size)
+
+    print('Validating')
+    if draw then
+        prediction = torch.zeros(self.loader.validation_size*self.opt.batch_size)
+        gt = torch.zeros(self.loader.validation_size*self.opt.batch_size)
+    end
 
     for i = 1,self.loader.validation_size do
         xlua.progress(i,self.loader.validation_size)
-        preds, targets = feval_val()
+        preds, targets, err = feval_val()
 
-        for j = 1,preds:size(1) do
-            local index = (i-1) * self.opt.batch_size + j
-            prediction[index] = preds[j]
-            gt[index] = targets[j]
+        if draw then
+            for j = 1,preds:size(1) do
+                local index = (i-1) * self.opt.batch_size + j
+                prediction[index] = preds[j]
+                gt[index] = targets[j]
+            end
         end
-
     end
 
-    gnuplot.figure()
-    gnuplot.plot({{'targets', gt, 'lines ls 1'},{'predictions', prediction, 'lines ls 2'}})
-    io.read()
+    if draw then
+        gnuplot.figure()
+        gnuplot.plot({{'targets', gt, 'lines ls 1'},{'predictions', prediction, 'lines ls 2'}})
+    end
+
+    return loss
 end
 
 
--- simple_LSTM_PoC.lua
+++ simple_LSTM_PoC.lua
@@ -5,7 +5,7 @@ require 'simple_LSTM'
 -- params
 cmd = torch.CmdLine()
 -- model params
-cmd:option('-rnn_size', 5, 'Size of LSTM internal state')
+cmd:option('-rnn_size', 50, 'Size of LSTM internal state')
 cmd:option('-window_size',15,'window size to look into the series')
 cmd:option('-feature_dims',1,'features of the time-series')
 -- optimization
@@ -13,20 +13,18 @@ cmd:option('-learning_rate', 1e-4, 'Learning rate')
 cmd:option('-learning_rate_decay', 0.95, 'Learning rate decay')
 cmd:option('-learning_rate_decay_after', 10, 'In number of epochs, when to start decaying the learning rate')
 cmd:option('-decay_rate', 0.95, 'Decay rate for rmsprop')
-cmd:option('-batch_size', 4, 'Batch size')
-cmd:option('-max_epochs', 50000, 'Number of full passes through the training data')
+cmd:option('-batch_size', 100, 'Batch size')
+cmd:option('-max_epochs', 5000, 'Number of full passes through the training data')
 cmd:option('-dropout', 0.5, 'Dropout')
 cmd:option('-init_from', '', 'Initialize network parameters from checkpoint at this path')
 -- bookkeeping
 cmd:option('-seed', 981723, 'Torch manual random number generator seed')
-cmd:option('-save_every', 1000, 'No. of iterations after which to checkpoint')
-cmd:option('-train_fc7_file', 'data/train_fc7.t7', 'Path to fc7 features of training set')
-cmd:option('-train_fc7_image_id_file', 'data/train_fc7_image_id.t7', 'Path to fc7 image ids of training set')
-cmd:option('-val_fc7_file', 'data/val_fc7.t7', 'Path to fc7 features of validation set')
-cmd:option('-val_fc7_image_id_file', 'data/val_fc7_image_id.t7', 'Path to fc7 image ids of validation set')
+cmd:option('-save_every', 10000, 'No. of iterations after which to checkpoint')
+cmd:option('-train_file', 'data/train_data.t7', 'Path to features of training set')
+cmd:option('-val_file', 'data/val_data.t7', 'Path to features of validation set')
 cmd:option('-data_dir', 'data', 'Data directory')
 cmd:option('-checkpoint_dir', 'checkpoints', 'Checkpoint directory')
-cmd:option('-savefile', 'vqa', 'Filename to save checkpoint to')
+cmd:option('-savefile', 'simple_lstm', 'Filename to save checkpoint to')
 -- gpu/cpu
 cmd:option('-gpuid', -1, '0-indexed id of GPU to use. -1 = CPU')
 
@@ -35,22 +33,23 @@ opt = cmd:parse(arg or {})
 
 
 -- create a data loader
--- if opt.feature_dims == 1 then
---     require 'Loader_series'
---     loader = Loader.new(opt.batch_size, opt.window_size)
--- else
+if opt.feature_dims == 1 then
+    require 'Loader_series'
+    loader = Loader.new(opt.batch_size, opt.window_size)
+else
     require 'Loader_multifeature_series'
     loader = Loader.new(opt.feature_dims, opt.batch_size, opt.window_size)
--- end
+end
 
 -- create the lstm
 my_lstm = LSTM.new(loader, opt)
 
-my_lstm:buildLSTM(opt.feature_dims,1)
+my_lstm:createRNN(opt.feature_dims,1)
 
 -- train the lstm
 my_lstm:train()
 
 -- evaluate the lstm
-my_lstm:validate()
+my_lstm:validate(true)
 
+io.read()
